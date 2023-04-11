35dc749b6c1aae73a67b1f4f537ddf95d7dc8f15
Sapna Jha <sapnajha1919@gmail.com>
2023-04-11 20:33:54 +0530

    Implemented basic structure for suggestion component to render when searchbar is clicked

---
+++ src/Components/ClothesGrid/ClothesGrid.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ClothesGrid.sass"
+export default function ClothesGrid({ clothesData }) {
+    return (
+      <div className="clothes-grid">
+        {clothesData.map((clothing, index) => (
+          <div key={index} className="clothing-item">
+            <img src={clothing.image} alt={clothing.name} className="cloth-image"/>
+            <p className="cloth-name">{clothing.name}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
--- src/Pages/SuggestionPage/Suggestion.js
+++ src/Pages/SuggestionPage/Suggestion.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Suggestion.sass"
+import ClothesGrid from "../../Components/ClothesGrid/ClothesGrid"
+import { faker } from '@faker-js/faker';
 export default function RecommendedPage() {
+  const clothesData = [];
+
+  for (let i = 0; i < 5; i++) {
+    // const seed = Math.floor(Math.random() * 100000);
+    // faker.seed(seed);
+    faker.seed(i);
+    const image = faker.image.imageUrl();
+    const name = faker.commerce.productName();
+    clothesData.push({ image, name });
+  }
   return (
     <div className="recommended-container">
-      
+      <p className="top-heading">Latest Trends</p>
+      <div>
+      <ClothesGrid clothesData={clothesData} />
+      </div>
+      <p className="sub-heading">Popular Suggestions</p>
+
     </div>
   )
 }
