c7f631dd73def31f3f0f4f30ea235f72648d5cb4
Sapna Jha <sapnajha1919@gmail.com>
2023-04-12 12:46:51 +0530

    Added wishlist heart and hover button on the images

--- src/Components/SearchClothGrid/SearchClothGrid.js
+++ src/Components/SearchClothGrid/SearchClothGrid.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import { faker } from '@faker-js/faker';
+import React, { useState } from 'react'
 import "./SearchClothGrid.sass"
 import ReactSimpleStarRating from "react-rating-stars-component";
-export default function SearchClothGrid({clothItems}) {
-    // const { imageUrl, clothName, price1, price2, number1, number2 } = props;
+import Heart from "react-heart"
+export default function SearchClothGrid({ clothItems }) {
+  const [active, setActive] = useState(false)
   return (
     <div className='clothing-grid'>
       {clothItems.map((cloth, index) => (
-          <div key={index} className="clothing-item">
-            <img src={cloth.imageUrl} alt={cloth.clothname} className="cloth-image"/>
-            <p className='cloth-name'>{cloth.clothName}</p>
-            <div className="price-tags">
+        <div key={index} className="clothing-item">
+          <img src={cloth.imageUrl} alt={cloth.clothname} className="cloth-image" />
+          <button className='hover-button'>View Product</button>
+          <Heart className="wishlist-heart" isActive={active} onClick={() => setActive(!active)} activeColor="red" inactiveColor="white" />
+          <p className='cloth-name'>{cloth.clothName}</p>
+          <div className="price-tags">
             <div className='price-1'>{`Rs. ${cloth.price1}`}</div>
             <div className='price-2'>{`Rs. ${cloth.price2}`}</div>
-            </div>
-            <div className='rate'>
+          </div>
+          <div className='rate'>
             <div className="cloth-name"><ReactSimpleStarRating emptyColor='#ffffff' size={20} value={cloth.number1} edit={false} /></div>
             <div className="review-no">{`(${cloth.number2})`}</div>
-            </div>
           </div>
-        ))}
+        </div>
+      ))}
     </div>
   )
 }
